b5cb1ef4c2f09fe49c64cdcf822b408b6cbd46de
Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>
2020-09-28 19:45:56 +0300

    Test add context to file has been done.

--- common/constants.py
+++ common/constants.py
@@ -1,6 +1,7 @@
 import os
 
 FILE_NAME = "file_name.txt"
+FILE_NAME_CONTEXT = "Easier to ask the forgiveness than permission."
 LIST_FILE_NAME = ["linux.txt", "windows.rtf", "macOS.doc"]
 TEMP_DIRECTORY = os.path.join("/tmp", "temp_directory")
 
